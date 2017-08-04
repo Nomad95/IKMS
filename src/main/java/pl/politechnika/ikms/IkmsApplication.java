944841f052c31fda951202ebbfe8efef848be2b6
igor-kopec <igokop99@gmail.com>
2017-08-04 17:34:14 +0200

    initial commit

---
+++ src/main/java/pl/politechnika/ikms/IkmsApplication.java
@@ -0,0 +1,12 @@
+package pl.politechnika.ikms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkmsApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/politechnika/ikms/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.politechnika.ikms;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IkmsApplication.class);
+	}
+
+}
