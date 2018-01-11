d9f4b986351dfb04db7a868639965dd1c407839f
Konrad Depta <konradd93@gmail.com>
2018-01-11 01:52:33 +0100

    Messages and notifications for mobile

--- IKMS-server/src/main/java/pl/politechnika/ikms/rest/dto/notification/NotificationsGroupedBySender.java
+++ IKMS-server/src/main/java/pl/politechnika/ikms/rest/dto/notification/NotificationsGroupedBySender.java
@@ -1,19 +1,4 @@
 package pl.politechnika.ikms.rest.dto.notification;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.List;
-@Data
-@AllArgsConstructor
 public class NotificationsGroupedBySender {
-
-        private Long senderId;
-
-        private String senderFullName;
-
-        private List<NotificationWithoutSenderDto> notifications;
-
-        private int numberOfUnread;
-
 }
